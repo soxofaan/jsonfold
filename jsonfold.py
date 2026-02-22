bd5fd50b98c1311327da09698098836efd2c1cf0
Stefaan Lippens <soxofaan@gmail.com>
2026-02-23 00:10:12 +0100

    Reduce duplication in logic a bit
    and more test coverage

--- jsonfold.py
+++ jsonfold.py
@@ -12,39 +12,39 @@ def fold_iter(
 
     for line in lines:
         stripped = line.strip()
+
         if stripped.endswith("{") or stripped.endswith("["):
             # Start a new level on the stack
-            stack.append([line])
-        elif stripped.startswith("}") or stripped.startswith("]"):
-            # Close current level: time to see if we can fold or not (collapse to multi-line)
-            if stack[-1]:
-                stack[-1].append(line)
+            stack.append([])
 
+        # Yield immediately if current level has collapsed already
+        if not stack or stack[-1] is None:
+            yield line
+        # otherwise: append to buffer and look deeper
+        else:
+            stack[-1].append(line)
+
+            if stripped in {"}", "},", "]", "],"}:
+                # Close current level: time to see if we can fold or not (collapse to multi-line)
                 closed = stack.pop()
                 # TODO: remove space after trailing element
                 folded = closed[0] + " ".join(s.strip() for s in closed[1:])
+
                 if len(folded) > max_width:
-                    # Collapse all levels:
+                    # Collapse all levels (if not already):
                     for level in range(len(stack)):
                         if stack[level]:
                             yield from stack[level]
                             # Mark level as collapsed
+                            # TODO: just remove level from stack instead of setting it None?
                             stack[level] = None
                     yield from closed
                 else:
-                    # Move folded result up one level
+                    # Move folded result up one level (unless it's collapsed already)
                     if stack and stack[-1]:
                         stack[-1].append(folded)
                     else:
                         yield folded
-            else:
-                yield line
-        else:
-            # Append to current buffer
-            if stack and stack[-1]:
-                stack[-1].append(line)
-            else:
-                yield line
 
 
 def fold(encoded: str, max_width: int = _DEFAULT_MAX_WIDTH) -> str:
@@ -53,37 +53,3 @@ def fold(encoded: str, max_width: int = _DEFAULT_MAX_WIDTH) -> str:
 
 def dumps(obj, max_width: int = _DEFAULT_MAX_WIDTH) -> str:
     return fold(json.dumps(obj=obj, indent=2), max_width=max_width)
-
-
-if __name__ == "__main__":
-    data = {
-        "foo": [1, 2, 3, 4, 5, 6],
-        "bar": [x * 1111 for x in range(10)],
-        "meh": {
-            "a": {
-                "bb": {
-                    "ccc": {
-                        "dddd": {
-                            "eeeee": {
-                                "ffffff": 0,
-                            }
-                        }
-                    },
-                    "CCC": {
-                        "D": 13,
-                        "DD": 133,
-                        "DDD": 1333,
-                    },
-                }
-            }
-        },
-    }
-
-    lines = json.dumps(data, indent=2).split("\n")
-    print(f"{lines=}")
-
-    for max_width in [20, 40, 80, 120]:
-        print(f"{max_width=}")
-
-        for res in fold(lines=lines, max_width=max_width):
-            print(f"[{len(res):4d}] {res=} ")
--- test_jsonfold.py
+++ test_jsonfold.py
@@ -1,3 +1,6 @@
+import textwrap
+import pytest
+
 from jsonfold import dumps
 
 
@@ -35,6 +38,7 @@ def test_dumps_empty_dict():
 def test_dumps_flat_list():
     assert dumps([1, 2, 3, 4, 5]) == "[1, 2, 3, 4, 5 ]"
     assert dumps([1, 2, 3, 4, 5], max_width=10) == "[\n  1,\n  2,\n  3,\n  4,\n  5\n]"
+    assert dumps([1, 2, 3, 4, 5], max_width=1) == "[\n  1,\n  2,\n  3,\n  4,\n  5\n]"
 
 
 def test_dumps_flat_dict():
@@ -46,3 +50,259 @@ def test_dumps_flat_dict():
         dumps({"name": "alice", "color": "green"}, max_width=10)
         == '{\n  "name": "alice",\n  "color": "green"\n}'
     )
+    assert (
+        dumps({"name": "alice", "color": "green"}, max_width=1)
+        == '{\n  "name": "alice",\n  "color": "green"\n}'
+    )
+
+
+@pytest.mark.parametrize(
+    ["max_width", "obj", "expected"],
+    [
+        (
+            80,
+            {"five": list(range(5)), "ten": list(range(10))},
+            '{"five": [0, 1, 2, 3, 4 ], "ten": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] }',
+        ),
+        (
+            40,
+            {"five": list(range(5)), "ten": list(range(10))},
+            """\
+            {
+              "five": [0, 1, 2, 3, 4 ],
+              "ten": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+            }""",
+        ),
+        (
+            30,
+            {"five": list(range(5)), "ten": list(range(10))},
+            """\
+            {
+              "five": [0, 1, 2, 3, 4 ],
+              "ten": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ]
+            }""",
+        ),
+        (
+            80,
+            {str(x): chr(97 + x) * x for x in range(5)},
+            '{"0": "", "1": "b", "2": "cc", "3": "ddd", "4": "eeee" }',
+        ),
+        (
+            40,
+            {str(x): chr(97 + x) * x for x in range(5)},
+            """\
+            {
+              "0": "",
+              "1": "b",
+              "2": "cc",
+              "3": "ddd",
+              "4": "eeee"
+            }""",
+        ),
+    ],
+)
+def test_dumps_listings(max_width, obj, expected):
+    expected = textwrap.dedent(expected)
+    assert dumps(obj, max_width=max_width) == expected
+
+
+@pytest.mark.parametrize(
+    ["max_width", "obj", "expected"],
+    [
+        (
+            80,
+            {
+                "query": "get stuff",
+                "results": {
+                    "count": 5,
+                    "data": [
+                        {"id": 1, "name": "Alice", "payments": None},
+                        {"id": 23, "name": "Bob", "payments": [100, 200]},
+                        {
+                            "id": 3000,
+                            "name": "Carol",
+                            "status": "premium",
+                            "payments": [1000, 3000, 2000, 2, -5],
+                        },
+                        {"id": 44, "name": "Dave", "payments": [1, 5]},
+                        {
+                            "id": 555,
+                            "name": "Eric",
+                            "payments": [44, {"price": 666, "currency": "tulip bulbs"}],
+                        },
+                    ],
+                },
+                "_id": "123kthxbye",
+            },
+            """\
+            {
+              "query": "get stuff",
+              "results": {
+                "count": 5,
+                "data": [
+                  {"id": 1, "name": "Alice", "payments": null },
+                  {"id": 23, "name": "Bob", "payments": [100, 200 ] },
+                  {
+                    "id": 3000,
+                    "name": "Carol",
+                    "status": "premium",
+                    "payments": [1000, 3000, 2000, 2, -5 ]
+                  },
+                  {"id": 44, "name": "Dave", "payments": [1, 5 ] },
+                  {
+                    "id": 555,
+                    "name": "Eric",
+                    "payments": [44, {"price": 666, "currency": "tulip bulbs" } ]
+                  }
+                ]
+              },
+              "_id": "123kthxbye"
+            }""",
+        ),
+        (
+            120,
+            {
+                "query": "get stuff",
+                "results": {
+                    "count": 5,
+                    "data": [
+                        {"id": 1, "name": "Alice", "payments": None},
+                        {"id": 23, "name": "Bob", "payments": [100, 200]},
+                        {
+                            "id": 3000,
+                            "name": "Carol",
+                            "status": "premium",
+                            "payments": [1000, 3000, 2000, 2, -5],
+                        },
+                        {"id": 44, "name": "Dave", "payments": [1, 5]},
+                        {
+                            "id": 555,
+                            "name": "Eric",
+                            "payments": [44, {"price": 666, "currency": "tulip bulbs"}],
+                        },
+                    ],
+                },
+                "_id": "123kthxbye",
+            },
+            """\
+            {
+              "query": "get stuff",
+              "results": {
+                "count": 5,
+                "data": [
+                  {"id": 1, "name": "Alice", "payments": null },
+                  {"id": 23, "name": "Bob", "payments": [100, 200 ] },
+                  {"id": 3000, "name": "Carol", "status": "premium", "payments": [1000, 3000, 2000, 2, -5 ] },
+                  {"id": 44, "name": "Dave", "payments": [1, 5 ] },
+                  {"id": 555, "name": "Eric", "payments": [44, {"price": 666, "currency": "tulip bulbs" } ] }
+                ]
+              },
+              "_id": "123kthxbye"
+            }""",
+        ),
+        (
+            80,
+            {
+                "a": {
+                    "bb": {
+                        "ccc": {
+                            "dddd": {
+                                "eeeee": {
+                                    "ffffff": "foo",
+                                },
+                            }
+                        },
+                        "CCC": {
+                            "D": 13,
+                            "DD": 133,
+                            "DDD": 1333,
+                        },
+                    }
+                }
+            },
+            """\
+            {
+              "a": {
+                "bb": {
+                  "ccc": {"dddd": {"eeeee": {"ffffff": "foo" } } },
+                  "CCC": {"D": 13, "DD": 133, "DDD": 1333 }
+                }
+              }
+            }""",
+        ),
+        (
+            120,
+            {
+                "a": {
+                    "bb": {
+                        "ccc": {
+                            "dddd": {
+                                "eeeee": {
+                                    "ffffff": "foo",
+                                },
+                            }
+                        },
+                        "CCC": {
+                            "D": 13,
+                            "DD": 133,
+                            "DDD": 1333,
+                        },
+                    }
+                }
+            },
+            '{"a": {"bb": {"ccc": {"dddd": {"eeeee": {"ffffff": "foo" } } }, "CCC": {"D": 13, "DD": 133, "DDD": 1333 } } } }',
+        ),
+        (
+            40,
+            {
+                "a": {
+                    "bb": {
+                        "ccc": {
+                            "dddd": {
+                                "eeeee": {
+                                    "ffffff": "foo",
+                                },
+                            }
+                        },
+                        "CCC": {
+                            "D": 13,
+                            "DD": 133,
+                            "DDD": 1333,
+                        },
+                    }
+                }
+            },
+            """\
+            {
+              "a": {
+                "bb": {
+                  "ccc": {
+                    "dddd": {
+                      "eeeee": {"ffffff": "foo" }
+                    }
+                  },
+                  "CCC": {
+                    "D": 13,
+                    "DD": 133,
+                    "DDD": 1333
+                  }
+                }
+              }
+            }""",
+        ),
+    ],
+)
+def test_dumps_deep_nesting(obj, max_width, expected):
+    expected = textwrap.dedent(expected)
+    assert dumps(obj, max_width=max_width) == expected
