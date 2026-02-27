3fe8acc8e3262ffd4c3b9e074d2ee46de8684838
Stefaan Lippens <soxofaan@gmail.com>
2026-02-27 01:00:58 +0100

    Add some tests on the consume-flush dance of fold_iter

--- test_jsonfold.py
+++ test_jsonfold.py
@@ -1,7 +1,9 @@
+import json
 import textwrap
+from typing import Iterable, List
 import pytest
 
-from jsonfold import dumps
+from jsonfold import dumps, fold_iter
 
 
 def test_dumps_none():
@@ -306,3 +308,183 @@ def test_dumps_listings(max_width, obj, expected):
 def test_dumps_deep_nesting(obj, max_width, expected):
     expected = textwrap.dedent(expected)
     assert dumps(obj, max_width=max_width) == expected
+
+
+class TrackingIterator:
+    """
+    Wrapper for an iterable of strings, to keep track of what has been consumed already.
+    """
+
+    def __init__(self, items: Iterable[str]):
+        self._items = iter(items)
+        self.consumed = []
+        self._report_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self._items)
+        self.consumed.append(item)
+        return item
+
+    def new_consumed(self) -> List[str]:
+        """Report newly consumed items since the last call."""
+        index = self._report_index
+        self._report_index = len(self.consumed)
+        return self.consumed[index:]
+
+
+def test_tracking_iterator_basic():
+    iterator = TrackingIterator(["a", "b", "c"])
+
+    assert iterator.consumed == []
+    assert iterator.new_consumed() == []
+
+    assert next(iterator) == "a"
+    assert iterator.consumed == ["a"]
+    assert iterator.new_consumed() == ["a"]
+
+    assert next(iterator) == "b"
+    assert iterator.consumed == ["a", "b"]
+
+    assert next(iterator) == "c"
+    assert iterator.consumed == ["a", "b", "c"]
+    assert iterator.new_consumed() == ["b", "c"]
+    assert iterator.new_consumed() == []
+
+    with pytest.raises(StopIteration):
+        next(iterator)
+    assert iterator.consumed == ["a", "b", "c"]
+    assert iterator.new_consumed() == []
+
+
+def test_fold_iter_flushing_simple_one_line():
+    """Trivial case: everything fits on one line, so we should consume all input lines immediately."""
+    data = {"color": "green", "shape": "square"}
+    input_lines = TrackingIterator(json.dumps(data, indent=2).split("\n"))
+
+    folded = fold_iter(input_lines, max_width=80)
+    assert input_lines.consumed == []
+
+    line = next(folded)
+    assert line == '{"color": "green", "shape": "square"}'
+    assert input_lines.consumed == [
+        "{",
+        '  "color": "green",',
+        '  "shape": "square"',
+        "}",
+    ]
+
+
+def test_fold_iter_flushing_simple_multiline():
+    """Multi-line result, but just one level, so all lines should be consumed immediately."""
+    data = {"color": "green", "shape": "square"}
+    input_lines = TrackingIterator(json.dumps(data, indent=2).split("\n"))
+
+    folded = fold_iter(input_lines, max_width=20)
+    assert input_lines.new_consumed() == []
+
+    assert next(folded) == "{"
+    assert input_lines.new_consumed() == [
+        "{",
+        '  "color": "green",',
+        '  "shape": "square"',
+        "}",
+    ]
+
+    assert next(folded) == '  "color": "green",'
+    assert input_lines.new_consumed() == []
+
+    assert next(folded) == '  "shape": "square"'
+    assert input_lines.new_consumed() == []
+
+    assert next(folded) == "}"
+    assert input_lines.new_consumed() == []
+
+    with pytest.raises(StopIteration):
+        _ = next(folded)
+
+
+def test_fold_iter_flushing_nested():
+    """Multi-line result with nesting: input is consumed in chunks."""
+    data = {"three": list(range(3)), "five": list(range(5)), "ten": list(range(10))}
+    input_lines = TrackingIterator(json.dumps(data, indent=2).split("\n"))
+
+    folded = fold_iter(input_lines, max_width=25)
+    assert input_lines.new_consumed() == []
+
+    # Top level flush: while "three" would fit on one line, "five" would overflow.
+    assert next(folded) == "{"
+    assert input_lines.new_consumed() == [
+        "{",
+        '  "three": [',
+        "    0,",
+        "    1,",
+        "    2",
+        "  ],",
+        # TODO: with the next line, it should already be possible to determine that "five" won't fit,
+        #       and it's already time to flush without further consumption.
+        '  "five": [',
+        "    0,",
+        "    1,",
+        "    2,",
+        "    3,",
+        "    4",
+        "  ],",
+    ]
+
+    # "three" fits on one line
+    assert next(folded) == '  "three": [0, 1, 2],'
+    assert input_lines.new_consumed() == []
+
+    # "five" doesn't fit, so we get multiple lines
+    assert next(folded) == '  "five": ['
+    assert input_lines.new_consumed() == []
+
+    for x in range(4):
+        assert next(folded) == f"    {x},"
+        assert input_lines.new_consumed() == []
+
+    assert next(folded) == "    4"
+    assert input_lines.new_consumed() == []
+
+    assert next(folded) == "  ],"
+    assert input_lines.new_consumed() == []
+
+    # Time for "ten": also doesn't fit. Buffer is empty at this point, so we have to consume a bit too.
+    assert next(folded) == '  "ten": ['
+    assert input_lines.new_consumed() == [
+        '  "ten": [',
+        "    0,",
+        "    1,",
+        "    2,",
+        "    3,",
+        "    4,",
+        "    5,",
+        "    6,",
+        "    7,",
+        "    8,",
+        "    9",
+        "  ]",
+    ]
+
+    for x in range(9):
+        assert next(folded) == f"    {x},"
+        assert input_lines.new_consumed() == []
+
+    assert next(folded) == "    9"
+    assert input_lines.new_consumed() == []
+
+    assert next(folded) == "  ]"
+    assert input_lines.new_consumed() == []
+
+    # Final closing brace
+    assert next(folded) == "}"
+    assert input_lines.new_consumed() == [
+        "}",
+    ]
+
+    with pytest.raises(StopIteration):
+        _ = next(folded)
+    assert input_lines.new_consumed() == []
